efd18485d68ad56d101762138beeda5fd8b91e62
Marek Štefanko <marcostesvk@gmail.com>
2021-04-15 17:26:13 +0200

    Added Email functionality

--- server/helpers/email.js
+++ server/helpers/email.js
@@ -5,17 +5,17 @@ const nodemailerSendgrid = require('nodemailer-sendgrid');
 exports.sendEmailWithNodemailer = (req, res, emailData) => {
   const transporter = nodeMailer.createTransport(
     nodemailerSendgrid({
-      apiKey: '',
+      apiKey: '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',
     }),
   );
 
   return transporter
     .sendMail(emailData)
     .then((info) => {
-      console.log(`Message sent: ${info.response}`);
+      console.log(`Email poslaný: ${info.response}`);
       return res.json({
-        message: `Email has been sent to your email. Follow the instruction to activate your account`,
+        message: `Email bol úspešne poslaný`,
       });
     })
-    .catch((err) => console.log(`Problem sending email: ${err}`));
+    .catch((err) => console.log(`Problém s posielaním emailu: ${err}`));
 };
